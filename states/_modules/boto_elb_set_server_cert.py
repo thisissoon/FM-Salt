5a5e96b971a5a5ca62bdd6b99d0ee2c494f6ae19
Chris Reeves <hello@chris.reeves.io>
2016-03-18 11:28:52 +0000

    Correct boto settings

--- states/_modules/boto_elb_set_server_cert.py
+++ states/_modules/boto_elb_set_server_cert.py
@@ -42,7 +42,7 @@ def __virtual__():
 def __init__(opts):
     salt.utils.compat.pack_dunder(__name__)
     if HAS_BOTO:
-        __utils__['boto.assign_funcs'](__name__, 'iam', pack=__salt__)
+        __utils__['boto.assign_funcs'](__name__, 'elb', module='ec2.elb', pack=__salt__)
 
 
 def set(
