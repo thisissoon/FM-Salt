af6ba55ed8123b182c5a1763d9cc630c90b2d6ff
Chris Reeves <hello@chris.reeves.io>
2016-03-18 11:43:56 +0000

    Hopefully fix getting an ELB connection

--- states/_modules/boto_elb_set_server_cert.py
+++ states/_modules/boto_elb_set_server_cert.py
@@ -1,30 +1,39 @@
 # -*- coding: utf-8 -*-
 
-# Import Python libs
 from __future__ import absolute_import
-import logging
-import json
-import yaml
 
-# Import salt libs
-import salt.utils.compat
-import salt.utils.odict as odict
-import salt.utils.boto
+# Import Python libs
+import logging
+from distutils.version import LooseVersion as _LooseVersion  # pylint: disable=import-error,no-name-in-module
+import json
+import salt.ext.six as six
+
+log = logging.getLogger(__name__)
 
 # Import third party libs
-# pylint: disable=unused-import
-from salt.ext.six import string_types
-from salt.ext.six.moves.urllib.parse import unquote as _unquote  # pylint: disable=no-name-in-module
 try:
     import boto
-    import boto.iam
+    # connection settings were added in 2.33.0
+    required_boto_version = '2.33.0'
+    if (_LooseVersion(boto.__version__) <
+            _LooseVersion(required_boto_version)):
+        msg = 'boto_elb requires boto {0}.'.format(required_boto_version)
+        logging.debug(msg)
+        raise ImportError()
+    import boto.ec2
+    from boto.ec2.elb import HealthCheck
+    from boto.ec2.elb.attributes import AccessLogAttribute
+    from boto.ec2.elb.attributes import ConnectionDrainingAttribute
+    from boto.ec2.elb.attributes import ConnectionSettingAttribute
+    from boto.ec2.elb.attributes import CrossZoneLoadBalancingAttribute
     logging.getLogger('boto').setLevel(logging.CRITICAL)
     HAS_BOTO = True
 except ImportError:
     HAS_BOTO = False
-# pylint: enable=unused-import
 
-log = logging.getLogger(__name__)
+# Import Salt libs
+from salt.ext.six import string_types
+import salt.utils.odict as odict
 
 
 __virtualname__ = 'boto_elb_ssl_certificate'
@@ -35,14 +44,9 @@ def __virtual__():
     Only load if boto libraries exist.
     '''
     if not HAS_BOTO:
-        return (False, 'The boto_iam module could not be loaded: boto libraries not found')
+        return (False, "The boto_elb module cannot be loaded: boto library not found")
+    __utils__['boto.assign_funcs'](__name__, 'elb', module='ec2.elb', pack=__salt__)
     return True
-
-
-def __init__(opts):
-    salt.utils.compat.pack_dunder(__name__)
-    if HAS_BOTO:
-        __utils__['boto.assign_funcs'](__name__, 'elb', module='ec2.elb', pack=__salt__)
 
 
 def set(
